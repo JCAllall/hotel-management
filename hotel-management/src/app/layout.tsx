9fd687ae9bbd9d171774aa5c61d2fede5800fc9b
Juan Cruz Allall <allalljuancruz@gmail.com>
2024-02-01 01:46:37 -0300

    first

--- hotel-management/src/app/layout.tsx
+++ hotel-management/src/app/layout.tsx
@@ -2,12 +2,12 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const poppins = Poppins({ 
-  subsets: ["latin"], 
-  weight: [´400´, ´500´, ´700´, ´900´],
-  style: [ ´italic´, ´normal´],
-  variable: ´--font-poppins´,
- });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "700", "900"],
+  style: ["italic", "normal"],
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: "Hotel Management App",
